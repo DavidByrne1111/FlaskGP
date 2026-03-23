f9ed493a6cab263d2f5323ccde966e1bf086a473
DavidByrne1111 <124476136@umail.ucc.ie>
2026-03-23 15:47:23 +0000

    API Image

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_bud():
-    return 'Hello Bud!'
+    api_url = 'https://dog.ceo/api/breeds/image/random'
+    response = requests.get(api_url)
+    data = response.json()
 
+    return f'<img src="{data["message"]}" width="400">'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
